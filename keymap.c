d9d5857a80ee1503405550078b47414573f83a51
yotsuki134 <dyu134@gmail.com>
2025-06-30 14:40:26 +0800

    fixed mcuconf true value; changed keymap

--- keymap.c
+++ keymap.c
@@ -20,26 +20,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_tkl_ansi(
         KC_TAB,  KC_Q,    KC_W,      KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,     KC_BSPC,
         KC_CAPS, KC_A,    KC_S,      KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,               KC_ENT,
-        KC_LSFT, KC_SLSH, KC_Z,      KC_X,    KC_C,    KC_V,     KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_RSFT,
-        KC_LCTL, KC_LGUI, KC_LALT,                     KC_SPC,   MO(2),   KC_SPC,           KC_RALT,  KC_APP,   KC_RCTL
+        KC_LSFT, KC_Z,    KC_X,      KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,
+        KC_LCTL, KC_LGUI, KC_LALT,                     KC_SPC,   MO(2),   MO(1),            KC_RALT,  KC_APP,   KC_RCTL
     ),
     [1] = LAYOUT_tkl_ansi(
-        KC_TAB,  KC_Q,    KC_W,      KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,     KC_BSPC,
-        KC_CAPS, KC_A,    KC_S,      KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,               KC_ENT,
-        KC_LSFT, KC_SLSH, KC_Z,      KC_X,    KC_C,    KC_V,     KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_RSFT,
-        KC_LCTL, KC_LALT, KC_LGUI,                     KC_SPC,   MO(3),   KC_SPC,           KC_RGUI,  KC_APP,   KC_RCTL
+        KC_TAB,  KC_F1,   KC_F2,     KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_BSPC,
+        KC_CAPS, KC_F11,  KC_F12,    KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,               KC_ENT,
+        KC_LSFT, KC_DEL,  KC_PGUP,   KC_PGDN, KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,
+        KC_LCTL, KC_LGUI, KC_LALT,                     KC_SPC,   MO(3),   KC_SPC,           KC_RALT,  KC_APP,   KC_RCTL
     ),
     [2] = LAYOUT_tkl_ansi(
         KC_ESC,   KC_1,    KC_2,     KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     KC_BSLS,
-        KC_CAPS,  MD_BLE1, MD_BLE2,  MD_BLE3, MD_24G,  RGB_SAI,  RGB_VAI, RGB_SAD, KC_MINS, KC_EQL,             RGB_RTOG,
-        KC_LSFT,  KC_GRV,  RGB_MOD,  RGB_SPD, RGB_SPI, KC_V,     RGB_HUD, RGB_VAD, RGB_HUI, KC_COMM,  KC_UP,    MW_CH,
+        KC_CAPS,  MD_BLE1, MD_BLE2,  MD_BLE3, MD_24G,  RGB_SAI,  KC_SCLN, KC_QUOT, KC_MINS, KC_EQL,             RGB_RTOG,
+        KC_LSFT,  KC_GRV,  RGB_MOD,  RGB_SPD, RGB_SPI, KC_V,     KC_LBRK, KC_RBRK, KC_COMM, KC_DOT,   KC_UP,    MW_CH,
         U_EE_CLR, QK_WLO,  KC_LALT,                    QK_BAT,   KC_NO,   KC_SPC,           KC_LEFT,  KC_DOWN,  KC_RGHT
     ),
     [3] = LAYOUT_tkl_ansi(
         KC_ESC,   KC_1,    KC_2,     KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     KC_BSLS,
         KC_CAPS,  MD_BLE1, MD_BLE2,  MD_BLE3, MD_24G,  RGB_SAI,  RGB_VAI, RGB_SAD, KC_MINS, KC_EQL,             RGB_RTOG,
         KC_LSFT,  KC_GRV,  RGB_MOD,  RGB_SPD, RGB_SPI, KC_V,     RGB_HUD, RGB_VAD, RGB_HUI, KC_COMM,  KC_UP,    MW_CH,
-        U_EE_CLR, KC_LALT, KC_LGUI,                    QK_BAT,   KC_NO,   KC_SPC,           KC_LEFT,  KC_DOWN,  KC_RGHT
+        U_EE_CLR, KC_LALT, KC_LGUI,                    QK_BAT,   KC_NO,   KC_NO,            KC_LEFT,  KC_DOWN,  KC_RGHT
     )
 };
 
